1339654fd56698d678d503e0044d4d3ff35fbbc8
kaestel <kaestel@e-types.com>
2014-03-25 15:33:08 +0100

    new docs setup

---
+++ src/templates/www.footer.php
@@ -0,0 +1,19 @@
+	</div>
+
+	<div id="navigation">
+		<ul class="navigation">
+			<li class="download"><a href="/download">Download</a></li>
+			<li class="docs"><a href="/docs">Documentation</a></li>
+			<li class="demo"><a href="/demo">Demo</a></li>
+			<li class="about"><a href="/about">About</a></li>
+		</ul>
+	</div>
+
+	<div id="footer">
+		<p>&lt;aliens&gt;we are all&lt;/aliens&gt;</p>
+	</div>
+
+</div>
+
+</body>
+</html>
