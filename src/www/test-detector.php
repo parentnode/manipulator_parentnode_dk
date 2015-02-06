0827763f24632c267240a81b9525ab44538a5396
kaestel <kaestel@e-types.com>
2015-02-06 12:04:13 +0100

    Initial js detector test

---
+++ src/www/test-detector.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="EN">
+<head>
+	<!-- (c) & (p) parentnode.dk 2009-2014 //-->
+	<!-- All material protected by copyrightlaws, as if you didnt know //-->
+	<!-- If you want to help build the ultimate frontend-centered platform, visit parentnode.dk -->
+	<title>Manipulator - Don't be scared - It's just JavaScript</title>
+	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+	<meta name="keywords" content="JavaScript library object oo mvc js parentnode html css animation manipulation" />
+	<meta name="description" content="Manipulator is an extensive modular cross-device JavaScript manipulation library" />
+	<meta name="viewport" content="initial-scale=1, user-scalable=no" />
+	<meta name="apple-mobile-web-app-capable" content="yes" />
+	<meta name="apple-mobile-web-app-status-bar-style" content="black" />
+	<script type="text/javascript" src="/js/manipulator/src/beta-u-detector.js"></script>
+</head>
+
+<body class="front">
+
+<div id="page">
+
+
+</div>
+
+</body>
+</html>
