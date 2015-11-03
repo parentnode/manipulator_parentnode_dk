80e1bb3a099cd0171f4315460ff6e123af57e797
kaestel <martin@think.dk>
2015-11-03 14:42:37 +0100

    desktop light preparations

--- src/www/js/manipulator/src/u-request.js
+++ src/www/js/manipulator/src/u-request.js
@@ -331,7 +331,7 @@ Util.validateResponse = function(response){
 //		u.bug("response:" + response + ":" + u.nodeId(response.node) + ":" + response.status)
 
 		u.bug("status:" + response.status + ":" + u.nodeId(response.node));
-		// u.bug("responseText:" + response.responseText);
+		u.bug("responseText:" + response.responseText);
 
 		try {
 			// valid response status
