848a8e088707493d1067ead2ac90be41b7f182b8
kaestel <martin@think.dk>
2015-11-03 13:44:56 +0100

    desktop light preparations

--- src/www/js/manipulator/src/u-svg.js
+++ src/www/js/manipulator/src/u-svg.js
@@ -44,59 +44,59 @@ Util.svg = function(svg_object) {
 		svg = u._svg_cache[svg_object.name].cloneNode(true);
 	}
 
-	// svg was not found in cache
-	if(!svg) {
-		svg = document.createElementNS("http://www.w3.org/2000/svg", "svg");
-
-
-		// width and height settings
-		if(svg_object.title) {
-			svg.setAttributeNS(null, "title", svg_object.title);
-		}
-		if(svg_object.class) {
-			svg.setAttributeNS(null, "class", svg_object.class);
-		}
-
-		if(svg_object.width) {
-			svg.setAttributeNS(null, "width", svg_object.width);
-		}
-		if(svg_object.height) {
-			svg.setAttributeNS(null, "height", svg_object.height);
-		}
-		if(svg_object.id) {
-			svg.setAttributeNS(null, "id", svg_object.id);
-		}
-		if(svg_object.node) {
-			svg.node = svg_object.node;
-		}
-
-
-		// TODO: implement filters
-//		if(svg_object.filters[shape]) {}
-
-
-
-		// loop through shapes in svg object
-		for(shape in svg_object.shapes) {
-
-			Util.svgShape(svg, svg_object.shapes[shape]);
-
-		}
-
-
-		// store svg in cache if it has an id
-		if(svg_object.name) {
-			if(!u._svg_cache) {
-				u._svg_cache = {};
-			}
-			u._svg_cache[svg_object.name] = svg.cloneNode(true);
-		}
-	}
-
-	// if node is specified, append svg to node
-	if(svg_object.node) {
-		svg_object.node.appendChild(svg);
-	}
+// 	// svg was not found in cache
+// 	if(!svg) {
+// 		svg = document.createElementNS("http://www.w3.org/2000/svg", "svg");
+//
+//
+// 		// width and height settings
+// 		if(svg_object.title) {
+// 			svg.setAttributeNS(null, "title", svg_object.title);
+// 		}
+// 		if(svg_object.class) {
+// 			svg.setAttributeNS(null, "class", svg_object.class);
+// 		}
+//
+// 		if(svg_object.width) {
+// 			svg.setAttributeNS(null, "width", svg_object.width);
+// 		}
+// 		if(svg_object.height) {
+// 			svg.setAttributeNS(null, "height", svg_object.height);
+// 		}
+// 		if(svg_object.id) {
+// 			svg.setAttributeNS(null, "id", svg_object.id);
+// 		}
+// 		if(svg_object.node) {
+// 			svg.node = svg_object.node;
+// 		}
+//
+//
+// 		// TODO: implement filters
+// //		if(svg_object.filters[shape]) {}
+//
+//
+//
+// 		// loop through shapes in svg object
+// 		for(shape in svg_object.shapes) {
+//
+// 			Util.svgShape(svg, svg_object.shapes[shape]);
+//
+// 		}
+//
+//
+// 		// store svg in cache if it has an id
+// 		if(svg_object.name) {
+// 			if(!u._svg_cache) {
+// 				u._svg_cache = {};
+// 			}
+// 			u._svg_cache[svg_object.name] = svg.cloneNode(true);
+// 		}
+// 	}
+//
+// 	// if node is specified, append svg to node
+// 	if(svg_object.node) {
+// 		svg_object.node.appendChild(svg);
+// 	}
 
 
 	// return svg
@@ -116,19 +116,19 @@ Util.svg = function(svg_object) {
 *		"filter":"f1"
 *	},
 */
-// Util.svgShape = function(svg, svg_object) {
-//
-// 	// create svg shape
-// 	svg_shape = document.createElementNS("http://www.w3.org/2000/svg", svg_object["type"]);
-// 	svg_object["type"] = null;
-// 	delete svg_object["type"];
-//
-// 	// loop through the remaining details of shapes
-// 	for(detail in svg_object) {
-//
-// 		svg_shape.setAttributeNS(null, detail, svg_object[detail]);
-// 	}
-//
-// 	return svg.appendChild(svg_shape);
-//
-// }
+Util.svgShape = function(svg, svg_object) {
+
+	// create svg shape
+	svg_shape = document.createElementNS("http://www.w3.org/2000/svg", svg_object["type"]);
+	svg_object["type"] = null;
+	delete svg_object["type"];
+
+	// loop through the remaining details of shapes
+	for(detail in svg_object) {
+
+		svg_shape.setAttributeNS(null, detail, svg_object[detail]);
+	}
+
+	return svg.appendChild(svg_shape);
+
+}
