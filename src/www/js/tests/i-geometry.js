3d8d659e5944d7b01ed4777d6eaea1c7d458c7b8
kaestel <martin@think.dk>
2015-11-09 15:56:57 +0100

    debugging android

--- src/www/js/tests/i-geometry.js
+++ src/www/js/tests/i-geometry.js
@@ -217,6 +217,8 @@
 
 //			document.body.scrollTop = scroll_y;
 			window.scrollTo(scroll_x, scroll_y);
+			window.scrollTop = scroll_y;
+			window.scrollLeft = scroll_x;
 //			window.scroll(0, scroll_y);
 
 			test_scroll = function() {
