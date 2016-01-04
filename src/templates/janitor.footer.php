8f2094092524725e7e2b64e81c54c305f5496b3a
kaestel <martin@think.dk>
2016-01-04 22:53:16 +0100

    Updated backend

--- src/templates/janitor.footer.php
+++ src/templates/janitor.footer.php
@@ -1,18 +1,35 @@
 	</div>
 
 	<div id="navigation">
-		<ul>
-			<?= $HTML->link("Navigations", "/janitor/admin/navigation/list", array("wrapper" => "li.navigation")) ?>
-			<?= $HTML->link("Tags", "/janitor/admin/tag/list", array("wrapper" => "li.tags")) ?>
-			<?= $HTML->link("Users", "/janitor/admin/user/list", array("wrapper" => "li.user")) ?>
-
-			<?= $HTML->link("Profile", "/janitor/admin/profile", array("wrapper" => "li.profile")) ?>
+		<ul class="sections">
+			<li class="content">
+				<h3>Content</h3>
+				<ul>
+					<?= $HTML->link("Pages", "/janitor/admin/page/list", array("wrapper" => "li.page")) ?>
+					<?= $HTML->link("Todos", "/janitor/admin/todo/list/state/30", array("wrapper" => "li.todo")) ?>
+				</ul>
+			</li>
+			<li class="site">
+				<h3>Site</h3>
+				<ul>
+					<?= $HTML->link("Navigations", "/janitor/admin/navigation/list", array("wrapper" => "li.navigation")) ?>
+					<?= $HTML->link("Tags", "/janitor/admin/tag/list", array("wrapper" => "li.tags")) ?>
+					<?= $HTML->link("Log", "/janitor/admin/log/list", array("wrapper" => "li.logs")) ?>
+				</ul>
+			</li>
+			<li class="users">
+				<h3>Users</h3>
+				<ul>
+					<?= $HTML->link("Users", "/janitor/admin/user/list", array("wrapper" => "li.user")) ?>
+					<?= $HTML->link("Profile", "/janitor/admin/profile", array("wrapper" => "li.profile")) ?>
+				</ul>
+			</li>
 		</ul>
 	</div>
 
 	<div id="footer">
 		<ul class="servicenavigation">
-			<li class="copyright">Janitor, Manipulator, Modulator - parentNode - Copyright 2015</li>
+			<li class="copyright">Janitor, Manipulator, Modulator - parentNode - Copyright 2016</li>
 		</ul>
 	</div>
 </div>
