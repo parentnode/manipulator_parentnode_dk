f21f5848f96cccdeb06707b12e709c11a00ef07d
kaestel <martin@think.dk>
2016-01-06 00:48:17 +0100

    Not supported in older browsers

--- src/templates/tests/u-textscaler.php
+++ src/templates/tests/u-textscaler.php
@@ -7,6 +7,11 @@ $this->headerIncludes(array(
 
 <div class="scene i:scene">
 	<h1>Textscaler</h1>
+
+<? if(preg_match("/desktop_light|mobile_light|seo/", $this->segment(array("type" => "dev")))): ?>
+	<p class="nosupport">NOT SUPPORTED in <?= $this->segment(array("type" => "dev")) ?></p>
+<? else: ?>
+
 	<p>
 		Text below scales when you resize the browser window - in both height and width.
 	</p>
@@ -23,5 +28,8 @@ $this->headerIncludes(array(
 			Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
 		</p>
 	</div>
+
+<? endif;?>
+
 </div>
 <div class="comments"></div>
