83fdbc2e7ffba9b44015c6b3efaacef7750b321b
kaestel <martin@think.dk>
2016-04-10 17:22:45 +0200

    Added bundle builder for Appoint

---
+++ src/www/bundles/axpoint_com/lib/seg_smartphone_include.js
@@ -0,0 +1,18 @@
+document.write('<script type="text/javascript" src="/js/manipulator/src/u.js"></script>');
+document.write('<script type="text/javascript" src="/js/manipulator/src/u-debug.js"></script>');
+
+document.write('<script type="text/javascript" src="/js/manipulator/src/u-animation.js"></script>');
+document.write('<script type="text/javascript" src="/js/manipulator/src/u-cookie.js"></script>');
+document.write('<script type="text/javascript" src="/js/manipulator/src/u-date.js"></script>');
+document.write('<script type="text/javascript" src="/js/manipulator/src/u-dom.js"></script>');
+document.write('<script type="text/javascript" src="/js/manipulator/src/u-events.js"></script>');
+document.write('<script type="text/javascript" src="/js/manipulator/src/u-events-browser.js"></script>');
+document.write('<script type="text/javascript" src="/js/manipulator/src/u-events-movements.js"></script>');
+document.write('<script type="text/javascript" src="/js/manipulator/src/u-geometry.js"></script>');
+document.write('<script type="text/javascript" src="/js/manipulator/src/u-init.js"></script>');
+document.write('<script type="text/javascript" src="/js/manipulator/src/u-math.js"></script>');
+document.write('<script type="text/javascript" src="/js/manipulator/src/u-request.js"></script>');
+document.write('<script type="text/javascript" src="/js/manipulator/src/u-string.js"></script>');
+document.write('<script type="text/javascript" src="/js/manipulator/src/u-system.js"></script>');
+document.write('<script type="text/javascript" src="/js/manipulator/src/u-timer.js"></script>');
+document.write('<script type="text/javascript" src="/js/manipulator/src/u-url.js"></script>');
