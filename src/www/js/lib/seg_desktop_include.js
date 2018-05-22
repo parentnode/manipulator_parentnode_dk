7503db9865870cdd10093fb6c0d8305233f2a117
Martin Kæstel Nielsen <martin@think.dk>
2018-05-22 11:05:28 +0200

    Removed u-basics

--- src/www/js/lib/seg_desktop_include.js
+++ src/www/js/lib/seg_desktop_include.js
@@ -1,6 +1,5 @@
 document.write('<script type="text/javascript" src="http://parentnode.local/js/global/seg_desktop_include.js"></script>');
 
 document.write('<script type="text/javascript" src="http://parentnode.local/js/global/desktop/i-documentation.js"></script>');
-document.write('<script type="text/javascript" src="http://think.local/js/lib/u-basics.js"></script>');
 
 document.write('<script type="text/javascript" src="/js/lib/desktop/i-front.js"></script>');
