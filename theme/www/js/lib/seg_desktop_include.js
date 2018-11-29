fed6c5324390956792d0c67e74a32311f44398d8
ElisabethSH <e.haabegaard@gmail.com>
2018-11-29 12:58:07 +0100

    Update js-include paths

--- theme/www/js/lib/seg_desktop_include.js
+++ theme/www/js/lib/seg_desktop_include.js
@@ -1,6 +1,6 @@
-document.write('<script type="text/javascript" src="assets/parentnode-skin-default/js/seg_desktop.js"></script>');
+document.write('<script type="text/javascript" src="/assets/parentnode-skin-default/js/seg_desktop.js"></script>');
 
-document.write('<script type="text/javascript" src="assets/parentnode-skin-default/js/lib/desktop/i-documentation.js"></script>');
+document.write('<script type="text/javascript" src="/assets/parentnode-skin-default/js/lib/desktop/i-documentation.js"></script>');
 
-document.write('<script type="text/javascript" src="js/lib/desktop/i-front.js"></script>');
-document.write('<script type="text/javascript" src="js/lib/u-settings.js"></script>');
+document.write('<script type="text/javascript" src="/js/lib/desktop/i-front.js"></script>');
+document.write('<script type="text/javascript" src="/js/lib/u-settings.js"></script>');
