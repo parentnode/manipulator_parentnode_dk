e307c7e9721adf4b4f919374cda04489b3fa3b33
threepwoodd <scope.dk@gmail.com>
2018-12-05 11:51:53 +0100

    Update includes for documentation

--- theme/www/js/lib/seg_desktop_include.js
+++ theme/www/js/lib/seg_desktop_include.js
@@ -4,7 +4,9 @@ document.write('<script type="text/javascript" src="/assets/parentnode-skin-defa
 
 document.write('<script type="text/javascript" src="/assets/parentnode-skin-default/js/lib/desktop/i-login.js"></script>');
 document.write('<script type="text/javascript" src="/assets/parentnode-skin-default/js/lib/desktop/i-documentation.js"></script>');
+
 document.write('<script type="text/javascript" src="/assets/parentnode-skin-default/js/manipulator/src/beta-u-fontsready.js"></script>');
+document.write('<script type="text/javascript" src="/assets/parentnode-skin-default/js/manipulator/src/u-form-builder.js"></script>');
 
 // Project
 document.write('<script type="text/javascript" src="/js/lib/desktop/i-front.js"></script>');
--- theme/www/js/lib/seg_smartphone_include.js
+++ theme/www/js/lib/seg_smartphone_include.js
@@ -5,5 +5,7 @@ document.write('<script type="text/javascript" src="/assets/parentnode-skin-defa
 document.write('<script type="text/javascript" src="/assets/parentnode-skin-default/js/lib/smartphone/i-login.js"></script>');
 document.write('<script type="text/javascript" src="/assets/parentnode-skin-default/js/lib/smartphone/i-documentation.js"></script>');
 
+document.write('<script type="text/javascript" src="/assets/parentnode-skin-default/js/manipulator/src/u-form-builder.js"></script>');
+
 // Project
 document.write('<script type="text/javascript" src="/js/lib/u-settings.js"></script>');
