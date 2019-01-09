950d2acf2cf80e0e084c98e9a4bcca27d30b97ee
threepwoodd <scope.dk@gmail.com>
2019-01-09 10:06:42 +0100

    Add u-form-custom to smartphone parentnode-skin

--- theme/www/bundles/parentnode-skin-default/lib/seg_smartphone_include.js
+++ theme/www/bundles/parentnode-skin-default/lib/seg_smartphone_include.js
@@ -9,6 +9,7 @@ document.write('<script type="text/javascript" src="/js/manipulator/src/u-events
 document.write('<script type="text/javascript" src="/js/manipulator/src/u-events-browser.js"></script>');
 document.write('<script type="text/javascript" src="/js/manipulator/src/u-events-movements.js"></script>');
 document.write('<script type="text/javascript" src="/js/manipulator/src/u-form.js"></script>');
+document.write('<script type="text/javascript" src="/js/manipulator/src/u-form-custom.js"></script>');
 document.write('<script type="text/javascript" src="/js/manipulator/src/u-geometry.js"></script>');
 document.write('<script type="text/javascript" src="/js/manipulator/src/u-init.js"></script>');
 document.write('<script type="text/javascript" src="/js/manipulator/src/u-math.js"></script>');
