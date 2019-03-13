0964a2fa81312a96e277deb8b2ca5671a5d7da0e
Martin Kæstel Nielsen <martin@think.dk>
2019-03-13 19:50:50 +0100

    Fix dev include

--- theme/www/js/dev/seg_desktop_light_include.js
+++ theme/www/js/dev/seg_desktop_light_include.js
@@ -2,7 +2,7 @@ document.write('<script type="text/javascript" src="/js/manipulator/src/u.js"></
 document.write('<script type="text/javascript" src="/js/manipulator/src/u-debug.js"></script>');
 
 //document.write('<script type="text/javascript" src="/js/manipulator/src/u-animation.js"></script>');
-document.write('<script type="text/javascript" src="/js/manipulator/src/u-audio.js"></script>');
+// document.write('<script type="text/javascript" src="/js/manipulator/src/u-audio.js"></script>');
 document.write('<script type="text/javascript" src="/js/manipulator/src/u-cookie.js"></script>');
 document.write('<script type="text/javascript" src="/js/manipulator/src/u-date.js"></script>');
 document.write('<script type="text/javascript" src="/js/manipulator/src/u-dom.js"></script>');
@@ -37,7 +37,7 @@ document.write('<script type="text/javascript" src="/js/manipulator/src/u-templa
 document.write('<script type="text/javascript" src="/js/manipulator/src/u-textscaler.js"></script>');
 document.write('<script type="text/javascript" src="/js/manipulator/src/u-timer.js"></script>');
 document.write('<script type="text/javascript" src="/js/manipulator/src/u-url.js"></script>');
-document.write('<script type="text/javascript" src="/js/manipulator/src/u-video.js"></script>');
+// document.write('<script type="text/javascript" src="/js/manipulator/src/u-video.js"></script>');
 
 
 //document.write('<script type="text/javascript" src="/js/manipulator/src/u-animation-desktop_light.js"></script>');
