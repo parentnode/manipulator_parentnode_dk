14522dba15126299ac7a7da5ef30fe9a7bcfb8c3
Martin Kæstel Nielsen <martin@think.dk>
2019-04-08 21:57:33 +0200

    Update tests

--- theme/templates/tests/u-fontsready.php
+++ theme/templates/tests/u-fontsready.php
@@ -1,45 +1,19 @@
-<style type="text/css">
-	.scene div {margin: 0 0 5px;}
-	.correct {background: green;}
-	.error {background: red;}
-</style>
+<?php
+$this->headerIncludes(array(
+	"/js/manipulator/src/beta-u-fontsready.js",
 
-<script type="text/javascript">
+	"/js/tests/i-fontsready.js",
+));
+?>
 
-	Util.Objects["test"] = new function() {
-		this.init = function(scene) {
-
-			scene.fontsLoaded = function() {
-				u.bug("fonts are ready");
-			}
-			scene.fontsNotLoaded = function() {
-				u.bug("fonts are not ready");
-			}
-			u.fontsReady(scene, [
-				{"family":"OpenSans", "style":"italic", "weight": 600},
-				{"family":"PT Serif", "weight": 700},
-				{"family":"OpenSans", "style":"normal", "weight": 100},
-			]);
-
-
-			scene.allGood = function() {
-				u.bug("fonts are ALL good");
-			}
-			scene.notGood = function() {
-				u.bug("fonts are NOT good");
-			}
-			u.fontsReady(scene, {"family":"PT Serif", "weight": 100}, {"callback":"allGood", "timeout":"notGood", "max":1000});
-
-		}
-	}
-
-</script>
-
-<div class="scene i:test">
+<div class="scene i:scene">
 	<h1>Fonts Ready</h1>
 	<p>
 		Checking if fonts are ready (loaded).
+
 	</p>
+
+	<div class="tests i:fontsready"></div>
 
 </div>
 <div class="comments"></div>
