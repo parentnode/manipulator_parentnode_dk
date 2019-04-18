474b8741360218cdac8689dcaf24bb014aac9522
Martin Kæstel Nielsen <martin@think.dk>
2019-04-18 08:49:22 +0200

    Update def

--- theme/www/bundles/parentnode-skin-default/lib/seg_desktop_light_include.js
+++ theme/www/bundles/parentnode-skin-default/lib/seg_desktop_light_include.js
@@ -5,11 +5,9 @@ document.write('<script type="text/javascript" src="/js/manipulator/src/u-cookie
 document.write('<script type="text/javascript" src="/js/manipulator/src/u-dom.js"></script>');
 document.write('<script type="text/javascript" src="/js/manipulator/src/u-events.js"></script>');
 document.write('<script type="text/javascript" src="/js/manipulator/src/u-events-browser.js"></script>');
-document.write('<script type="text/javascript" src="/js/manipulator/src/u-form.js"></script>');
 document.write('<script type="text/javascript" src="/js/manipulator/src/u-geometry.js"></script>');
 document.write('<script type="text/javascript" src="/js/manipulator/src/u-init.js"></script>');
 document.write('<script type="text/javascript" src="/js/manipulator/src/u-math.js"></script>');
-document.write('<script type="text/javascript" src="/js/manipulator/src/u-scrollto.js"></script>');
 document.write('<script type="text/javascript" src="/js/manipulator/src/u-string.js"></script>');
 document.write('<script type="text/javascript" src="/js/manipulator/src/u-system.js"></script>');
 document.write('<script type="text/javascript" src="/js/manipulator/src/u-timer.js"></script>');
@@ -19,6 +17,7 @@ document.write('<script type="text/javascript" src="/js/manipulator/src/u-txt.js
 document.write('<script type="text/javascript" src="/js/manipulator/src/u-array-desktop_light.js"></script>');
 document.write('<script type="text/javascript" src="/js/manipulator/src/u-dom-desktop_ie.js"></script>');
 document.write('<script type="text/javascript" src="/js/manipulator/src/u-dom-desktop_light.js"></script>');
+document.write('<script type="text/javascript" src="/js/manipulator/src/u-form-desktop_light.js"></script>');
 document.write('<script type="text/javascript" src="/js/manipulator/src/u-events-desktop_light.js"></script>');
 document.write('<script type="text/javascript" src="/js/manipulator/src/u-geometry-desktop_light.js"></script>');
 document.write('<script type="text/javascript" src="/js/manipulator/src/u-json-desktop_light.js"></script>');
--- theme/www/bundles/parentnode-skin-default/lib/seg_mobile_include.js
+++ theme/www/bundles/parentnode-skin-default/lib/seg_mobile_include.js
@@ -8,13 +8,16 @@ document.write('<script type="text/javascript" src="/js/manipulator/src/u-events
 document.write('<script type="text/javascript" src="/js/manipulator/src/u-geometry.js"></script>');
 document.write('<script type="text/javascript" src="/js/manipulator/src/u-init.js"></script>');
 document.write('<script type="text/javascript" src="/js/manipulator/src/u-math.js"></script>');
+document.write('<script type="text/javascript" src="/js/manipulator/src/u-string.js"></script>');
 document.write('<script type="text/javascript" src="/js/manipulator/src/u-system.js"></script>');
+document.write('<script type="text/javascript" src="/js/manipulator/src/u-timer.js"></script>');
 document.write('<script type="text/javascript" src="/js/manipulator/src/u-txt.js"></script>');
 
 
 document.write('<script type="text/javascript" src="/js/manipulator/src/u-array-desktop_light.js"></script>');
 document.write('<script type="text/javascript" src="/js/manipulator/src/u-dom-desktop_ie.js"></script>');
 document.write('<script type="text/javascript" src="/js/manipulator/src/u-dom-desktop_light.js"></script>');
+document.write('<script type="text/javascript" src="/js/manipulator/src/u-form-desktop_light.js"></script>');
 document.write('<script type="text/javascript" src="/js/manipulator/src/u-events-desktop_light.js"></script>');
 document.write('<script type="text/javascript" src="/js/manipulator/src/u-geometry-desktop_light.js"></script>');
 document.write('<script type="text/javascript" src="/js/manipulator/src/u-json-desktop_light.js"></script>');
