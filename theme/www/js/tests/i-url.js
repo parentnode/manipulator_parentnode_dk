44b57226964848dcf770ff240b99e94d156bff9a
Martin Kæstel Nielsen <martin@think.dk>
2019-08-30 14:15:54 +0200

    Extended edgecase testing

--- theme/www/js/tests/i-url.js
+++ theme/www/js/tests/i-url.js
@@ -15,20 +15,39 @@ Util.Objects["url"] = new function() {
 			// u.bug("test2 with url:" + u.getVar("test2", location.href + "?test1=get_this&test2=get_that"));
 			// u.bug("test3 with url:" + u.getVar("test3", location.href + "?test1=get_this&test2=get_that#test1=fake_param&test3=fake_param"));
 
+
+			// Using location.search
+
 			if(u.getVar("test1") == "get_this" 
 				&& u.getVar("test2") == "get_that" 
 				&& !u.getVar("test3")
-
-				&& u.getVar("test1", location.href + "?test1=get_this&test2=get_that#test1=fake_param&test3=fake_param") == "get_this" 
-				&& u.getVar("test2", location.href + "?test1=get_this&test2=get_that#test1=fake_param&test3=fake_param") == "get_that" 
-				&& !u.getVar("test3", location.href + "?test1=get_this&test2=get_that#test1=fake_param&test3=fake_param")
 			) {
-				u.ae(div, "div", {"class":"testpassed", "html":"u.getVar: correct"});
-				div.test_results["u.browser"] = true;
+				u.ae(div, "div", {"class":"testpassed", "html":"u.getVar (location.href): correct"});
+				div.test_results["u.url-location"] = true;
 			}
 			else {
-				u.ae(div, "div", {"class":"testfailed", "html":"u.getVar: error"});
-				div.test_results["u.url"] = false;
+				u.ae(div, "div", {"class":"testfailed", "html":"u.getVar (location.href): error"});
+				div.test_results["u.url-location"] = false;
+			}
+
+
+
+			// Passing string
+			var parameter_string = "test1=get_this&test2=get_that#test1=fake_param&test3=fake_param";
+
+			if(
+				u.getVar("test1", parameter_string) == "get_this" 
+				&& u.getVar("test1", "?" + parameter_string) == "get_this" 
+				&& u.getVar("test1", location.href + "?" + parameter_string) == "get_this"
+				&& u.getVar("test2", "?" + parameter_string) == "get_that" 
+				&& !u.getVar("test3", location.href + "?" + parameter_string)
+			) {
+				u.ae(div, "div", {"class":"testpassed", "html":"u.getVar (string): correct"});
+				div.test_results["u.url-string"] = true;
+			}
+			else {
+				u.ae(div, "div", {"class":"testfailed", "html":"u.getVar (string): error"});
+				div.test_results["u.url-string"] = false;
 			}
 		}
 	}
