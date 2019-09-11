3b1adf6734e7a1c1f2537c28c66562ee23cd7e58
Guybrush <36038081+Threepwoodd@users.noreply.github.com>
2019-09-11 18:00:27 +0200

    Halfway request test

--- theme/www/ajax/post-cors.php
+++ theme/www/ajax/post-cors.php
@@ -7,6 +7,7 @@ if(isset($read_access) && $read_access) {
 // If credientials are set to true (in js-request with credentials, origin cannot be *)
 header("Access-Control-Allow-Origin: ".$_SERVER['HTTP_ORIGIN']);
 header("Access-Control-Allow-Credentials: true");
+// header("Access-Control-Allow-Headers: content-type");
 
 
 $test = isset($_POST["test"]) ? $_POST["test"] : "";
