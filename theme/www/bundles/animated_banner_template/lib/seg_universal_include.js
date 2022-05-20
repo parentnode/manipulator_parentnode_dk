038dd86822f8068d0f6a2ecb9a74ddb309997b7a
kaestel <martin@think.dk>
2022-05-20 20:56:31 +0200

    Update bundles

---
+++ theme/www/bundles/animated_banner_template/lib/seg_universal_include.js
@@ -0,0 +1,8 @@
+document.write('<script type="text/javascript" src="/js/manipulator/src/u.js"></script>');
+
+document.write('<script type="text/javascript" src="/js/manipulator/src/u-dom.js"></script>');
+document.write('<script type="text/javascript" src="/js/manipulator/src/u-events.js"></script>');
+document.write('<script type="text/javascript" src="/js/manipulator/src/u-math.js"></script>');
+document.write('<script type="text/javascript" src="/js/manipulator/src/u-request.js"></script>');
+document.write('<script type="text/javascript" src="/js/manipulator/src/u-string.js"></script>');
+document.write('<script type="text/javascript" src="/js/manipulator/src/u-timer.js"></script>');
