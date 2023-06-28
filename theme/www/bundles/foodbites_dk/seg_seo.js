4f7c7d228c36c1ad2fd68c68cc3123fef10b78af
kaestel <martin@think.dk>
2023-06-28 14:32:36 +0200

    foodbites

---
+++ theme/www/bundles/foodbites_dk/seg_seo.js
@@ -0,0 +1,18 @@
+/*
+Manipulator v0.9.4-parentnode-skin-default Copyright 2023 https://manipulator.parentnode.dk
+js-merged @ 2023-06-28 09:58:39
+*/
+
+/*seg_seo_include.js*/
+
+/*u.js*/
+if(!u || !Util) {
+	var u, Util = u = new function() {};
+	u.version = "0.9.3";
+	u.bug = u.nodeId = u.exception = function() {};
+	u.stats = new function() {this.pageView = function(){};this.event = function(){};}
+	u.txt = function(index) {return index;}
+}
+function fun(v) {return (typeof(v) === "function")}
+function obj(v) {return (typeof(v) === "object")}
+function str(v) {return (typeof(v) === "string")}
